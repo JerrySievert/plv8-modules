f9687239c01d24228b03062e7240754290320a09
Jerry Sievert <code@legitimatesounding.com>
2016-04-17 13:29:59 -0700

    clean up test

--- test/tests.js
+++ test/tests.js
@@ -1,10 +1,7 @@
-function setup ( ) {
-  plv8.execute("SET plv8.start_proc = 'commonjs.plv8_startup';");
-}
 
 var tests = [
   {
-    'plv8.require should be in scope': function ( ) {
+    'require should be in scope': function ( ) {
       assert.equal(typeof require, 'function', 'require is a function');
     }
   }
